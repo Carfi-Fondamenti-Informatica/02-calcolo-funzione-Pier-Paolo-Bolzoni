d140556cf3fd24d64a45e914a4543860509ddc8c
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-04-15 23:47:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,21 @@ using namespace std;
 
  int main() {
 
-    float a, b, x,y;
+    float a=0, b=0, x=0, y=0, z=0;
 
     cin >> a >> b >> x >> y;
 
-    if (x < 0 && y > 0){
-        
-        cout << a * x - b * y << endl;
+    if ((x < 0) && (y > 0)){
+        z = a * x - b * y;
+        cout << z << endl;
 
-    } else if (x >= 0 && y <= 0){
-       
-    cout <<a * x * x + b * y << endl;
+    } else if ((x >= 0) && (y <= 0)){
+        z = a * x * x + b * y;
+    cout << z << endl;
     }
     else{
-        cout << a*x - b*y*y <<endl;
+        z = a*x - b*y*y;
+        cout << z <<endl;
     }
     return 0;
 }
